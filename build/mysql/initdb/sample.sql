23eb1d6d76ccb36f9d5cf52116b429296ec968df
mbo57 <96461698+mbo57@users.noreply.github.com>
2023-12-04 01:11:05 +0900

    fix: 初期データ作成SQLを修正

--- build/mysql/initdb/sample.sql
+++ build/mysql/initdb/sample.sql
@@ -1,21 +1,22 @@
 USE sample;
 
-DROP TABLE IF EXISTS task;
+DROP TABLE IF EXISTS todo;
 
-CREATE TABLE task
+CREATE TABLE todo
 (
-    id          INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name        VARCHAR(50),
+    id          INT           PRIMARY KEY AUTO_INCREMENT,
+    title       VARCHAR(50)   NOT NULL,
     description VARCHAR(1000),
-    created     DATETiME,
-    updated     DATETiME,
-    deadline    DATETiME,
+    created     DATETIME,
+    updated     DATETIME,
+    deadline    DATETIME,
     priority    VARCHAR(5),
     category    VARCHAR(50),
     status      VARCHAR(10),
     assignee    VARCHAR(20)
 );
 
-INSERT INTO task (name, description, created, updated, deadline, priority, category, status, assignee)
+INSERT INTO todo (title, description, created, updated, deadline, priority, category, status, assignee)
 VALUES ("買い物", "カレーの材料を買う", "2023-11-30 10:10:10", "2023-11-30 10:10:10", "2023-12-10 00:00:00", "高", "家事", "未完了", "母");
+INSERT INTO todo (title, description, created, updated, deadline, priority, category, status, assignee)
 VALUES ("散歩", "犬の散歩", "2023-11-30 10:10:10", "2023-11-30 10:10:10", "2023-11-30 12:00:00", "中", "その他", "未完了", "兄");
