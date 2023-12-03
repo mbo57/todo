612bb53d201215aa64c5505524b2114c699f02b7
mbo57 <96461698+mbo57@users.noreply.github.com>
2023-12-04 01:08:30 +0900

    add: 各層をmain関数で接続

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"app/internal/delivery/handlers"
+	"app/internal/delivery/routes"
+	"app/internal/repository"
+	"app/internal/repository/database"
+	"app/internal/usecase"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	db := database.NewDB()
+	todoRepositry := repository.NewTodoRepository(db)
+	todoUsecase := usecase.NewTodoUsecase(todoRepositry)
+	todoHandler := handlers.NewTodoHandler(todoUsecase)
+	e := routes.NewTodoRouter(todoHandler)
+	e.Logger.Fatal(e.Start(":8000"))
 }
